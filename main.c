fd4aa855cc1ba1e30950538d6643555f9431003c
Thais Lira <lirapolinario@gmail.com>
2024-03-31 00:08:39 -0300

    resolução do primeiro exercicio

---
+++ main.c
@@ -0,0 +1,26 @@
+// Considerando 2 números inteiros, calcule a soma, a subtração e a multiplicação entre eles.
+// Por fim, mostre os resultados das operações.
+
+#include <stdio.h> 
+#include <stdlib.h> 
+
+main (){
+
+        int num1, num2, soma, sub, mult; 
+
+        printf("digite um número:");
+        scanf ("%d", &num1);
+
+        printf("digite outro número:");
+        scanf ("%d", &num2);
+                
+        soma = num1 + num2;
+        sub = num1 - num2;
+        mult = num1 * num2;
+
+        printf("A soma dos dois números:  %d \n", soma);
+        printf("A subtração dos dois números:  %d \n", sub);
+        printf("A multiplicação dos dois números:  %d \n", mult);
+
+        system("pause");
+}
